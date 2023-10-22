9876790f886b45af54ba6c1dc637cb157da2ac20
thiago bueno garcia <thiagobuenogarcia@thiagos-Mini.home>
2023-10-21 22:24:10 -0400

    VERSION 1.2.0 - ADDED NEW FEATURE - Pick a vese on how you are feeling

--- get_verse.py
+++ get_verse.py
@@ -1,44 +1,8 @@
 import requests
-verse_me_ascii_art = r"""
+import json 
+import random
 
- _   _  _____ ______  _____  _____  ___  ___ _____ 
-| | | ||  ___|| ___ \/  ___||  ___| |  \/  ||  ___|
-| | | || |__  | |_/ /\ `--. | |__   | .  . || |__  
-| | | ||  __| |    /  `--. \|  __|  | |\/| ||  __| 
-\ \_/ /| |___ | |\ \ /\__/ /| |___  | |  | || |___ 
- \___/ \____/ \_| \_|\____/ \____/  \_|  |_/\____/ 
-                                                   
-"""
-
-oprions_ascii_art = r"""
-
-
-  ____  ___  ______________  _  ______
- / __ \/ _ \/_  __/  _/ __ \/ |/ / __/
-/ /_/ / ___/ / / _/ // /_/ /    /\ \  
-\____/_/    /_/ /___/\____/_/|_/___/  
-                                      
-"""
-
-found_it_ascii_art = r"""
-
-   _   _   _   _   _     _   _  
-  / \ / \ / \ / \ / \   / \ / \ 
- ( F | O | U | N | D ) ( I | T )
-  \_/ \_/ \_/ \_/ \_/   \_/ \_/ 
-
-"""
-
-bye_ascii_art = r"""
-
-   _____  ______
-  / _ ) \/ / __/
- / _  |\  / _/  
-/____/ /_/___/  
-                
-
-"""
-import requests
+from ascii import verse_me_ascii_art, oprions_ascii_art, found_it_ascii_art, bye_ascii_art
 
 class VerseMe:
     def __init__(self):
@@ -49,7 +13,7 @@ class VerseMe:
 
     def get_verse(self):
         print("\033[1m" + self.verse_me_ascii_art + "\033[0m")
-        print("\033[1m" + "WELCOME TO VERSE ME!" + "\033[1m" + "\n" + " Version 1.1")
+        print("\033[1m" + "WELCOME TO VERSE ME!" + "\033[1m" + "\n" + " Version 1.2")
 
         while True:
             user_input = input("Do you want to search a verse? Type 'More' for more options (y/n): ")
@@ -86,7 +50,8 @@ class VerseMe:
         print("\033[1m" + self.options_ascii_art + "\033[0m")
         print("1. Search for a verse")
         print("2. Random verse")
-        print("3. Exit")
+        print("3. Feeling the need for more?")
+        print("4. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -98,6 +63,8 @@ class VerseMe:
         elif choice == "2":
             self.get_random_verse()
         elif choice == "3":
+            VerseMe.Verse_mood().order_verse()
+        elif choice == "4":
             print("\033[1m" + self.bye_ascii_art + "\033[0m")
             exit()
         else:
@@ -111,7 +78,7 @@ class VerseMe:
             json_data = response.json()
             print(f"{self.found_it_ascii_art}")
 
-            book = json_data[0]['bookname']
+            book = json_data[0]['bookname'] 
             chapter = json_data[0]['chapter']
             verse = json_data[0]['verse']
             text = json_data[0]['text']
@@ -119,6 +86,133 @@ class VerseMe:
             print(f"Book: {book} || {chapter}:{verse} || Text: {text}")
         else:
             print(response.status_code, "Error")
+
+    class Verse_mood:
+        def __init__(self):
+            pass
+            
+        def order_verse(self):
+            
+            print("\033[1m" + "How are you feeling today?" + "\033[0m")
+            
+            print("\033[1m" + "1. Anger" + "\033[0m")
+            print("\033[1m" + "2. Patience" + "\033[0m")
+            print("\033[1m" + "3. Motivation" + "\033[0m")
+            print("\033[1m" + "4. Depression" + "\033[0m")
+            print("\033[1m" + "5. Upsmanship" + "\033[0m")
+            print("\033[1m" + "6. Love" + "\033[0m")
+            print("\033[1m" + "7. Obedience" + "\033[0m")
+            
+            choice = input("Enter your choice: ")
+            
+            if choice == "1" or choice == "Anger":
+                
+                self.anger_verse()
+                    
+            elif choice == "2" or choice == "Patience":
+
+                self.patience_verse()
+                    
+            elif choice == "3" or choice == "Motivation":
+                
+                self.motivation_verse()
+                    
+            elif choice == "4" or choice == "Depression":
+                
+                self.depression_verse()
+                    
+            elif choice == "5" or choice == "Uplifting":
+                
+                self.uplift_verse()
+                    
+            elif choice == "6" or choice == "Love":
+               
+                self.love_verse()
+                    
+            elif choice == "7" or choice == "Obedience":
+                
+                self.obedience_verse()
+                    
+            else:
+                
+                print("Invalid choice. Please choose a valid option")
+                
+        def anger_verse(self):
+            file_name = "verse_mood/data/data_verse_anger.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+            
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
+                
+        def patience_verse(self):
+            file_name = "verse_mood/data/data_verse_patience.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+                
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
+                
+        def motivation_verse(self):
+            file_name = "verse_mood/data/data_verse_motivation.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+                
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
+                
+        def depression_verse(self):
+            file_name = "verse_mood/data/data_verse_depression.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+                
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
+        
+        def uplift_verse(self):
+            file_name = "verse_mood/data/data_verse_uplifting.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+                
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
+                
+        def love_verse(self):
+            file_name = "verse_mood/data/data_verse_love.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+                
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
+                
+        def obedience_verse(self):
+            file_name = "verse_mood/data/data_verse_obedience.json"
+            
+            with open(file_name, 'r') as f:
+                verses = json.load(f)
+            
+            random_choice = random.choice(verses)
+            
+            print("Verse: " + random_choice['verse'])
+            print("Reference: " + random_choice['reference'])
 
 if __name__ == "__main__":
     verse_me = VerseMe()
